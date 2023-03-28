84a55ae08fcc071c9c19e88cfadbc3c0616e68a0
Joe Thornber <ejt@redhat.com>
2023-03-28 20:14:32 +0100

    [config] don't insist on by_id for /dev/mapper, also add override:
    disable_by_id_check

--- src/dmtest/config.py
+++ src/dmtest/config.py
@@ -4,14 +4,20 @@ import toml
 # Linux reordered my nvme drives once and I ran tests across
 # /boot.  This check tries to avoid that.  The exception is
 # virt devices, which don't seem to have an id.
-def check_dev(name, value):
-    if not value.startswith("/dev/vd") and not value.startswith("/dev/disk/by-id/"):
+def check_dev(cfg, name):
+    if cfg["disable_by_id_check"]:
+        return
+
+    value = cfg[name]
+    if not (
+        value.startswith("/dev/vd") or value.startswith("/dev/mapper/")
+    ) and not value.startswith("/dev/disk/by-id/"):
         raise ValueError(f"config value '{name}' does not begin with /dev/disk/by-id")
 
 
 def validate(cfg):
-    check_dev("metadata_dev", cfg["metadata_dev"])
-    check_dev("data_dev", cfg["data_dev"])
+    check_dev(cfg, "metadata_dev")
+    check_dev(cfg, "data_dev")
 
 
 def read_config(path="config.toml"):
