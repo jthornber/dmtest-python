972bf8da922c852a5984e436d5dac7f7754b94e8
Tony Asleson <tasleson@redhat.com>
2023-05-22 11:08:23 -0500

    Convert blk-archive test to dmtest-python test

--- src/dmtest/__main__.py
+++ src/dmtest/__main__.py
@@ -5,6 +5,7 @@ import dmtest.fixture
 import dmtest.process as process
 import dmtest.test_register as test_register
 import dmtest.blk_archive.rolling_snaps as blk_archive
+import dmtest.blk_archive.unit as blk_archive_unit
 import dmtest.thin.creation_tests as thin_creation
 import dmtest.thin.deletion_tests as thin_deletion
 import dmtest.thin.discard_tests as thin_discard
@@ -466,6 +467,7 @@ def main():
 
     tests = test_register.TestRegister()
     blk_archive.register(tests)
+    blk_archive_unit.register(tests)
     thin_creation.register(tests)
     thin_deletion.register(tests)
     thin_discard.register(tests)
