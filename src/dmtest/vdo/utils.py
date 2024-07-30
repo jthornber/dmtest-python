fe2363d50ec870c4a4fbb7d2032d2a70091e8c1b
Andrew Walsh <awalsh@redhat.com>
2024-07-30 17:33:58 -0400

    Moved fio to utils for generalized usage
    Updated compression value to have a default of 0, and then the
    previously existing test sets it to the previously used value of 74%.
    
    Signed-off-by: Andrew Walsh <awalsh@redhat.com>

--- src/dmtest/vdo/utils.py
+++ src/dmtest/vdo/utils.py
@@ -1,9 +1,31 @@
 import dmtest.process as process
 import dmtest.vdo.vdo_stack as vs
 import dmtest.vdo.status as status
+import logging as log
 
+import json
 import os
+import tempfile
 import time
+
+fio_config_template = """
+[stuff]
+randrepeat=1
+ioengine=libaio
+bs=4k
+size={size}
+rw=write
+direct=1
+scramble_buffers=1
+buffer_compress_percentage={compress}
+buffer_compress_chunk=4k
+filename={filename}
+iodepth=128
+offset={offset}
+end_fsync=0
+{maybe_verify}
+group_reporting=1
+"""
 
 def standard_stack(fix, **opts):
     cfg = fix.cfg
@@ -28,3 +50,25 @@ def fsync(dev):
     """Sync the specified device or file."""
     with open(dev, 'w') as thing:
         os.fsync(thing.fileno())
+
+def run_fio(dev, size, offset, verify = False, stats = True, compression = 0):
+    fio_config = fio_config_template.format(size=size,
+                                            offset=offset,
+                                            compress=compression,
+                                            filename=str(dev),
+                                            maybe_verify = "verify_only" if verify else "")
+    log.info("fio config:\n" + fio_config)
+    with tempfile.NamedTemporaryFile('w') as conf:
+        conf.write(fio_config)
+        conf.flush()
+        if stats:
+            with tempfile.NamedTemporaryFile('w') as out:
+                process.run(f"fio {conf.name} --output={out.name} --output-format=json+")
+                fio_out = json.load(open(out.name, 'r'))
+                written = fio_out['jobs'][0]['write']['io_bytes'] # bytes
+                duration = fio_out['jobs'][0]['write']['runtime'] # msec
+                #log.info(fio_out)
+                log.info(f"wrote {written} bytes in {duration} msec")
+                return fio_out
+        else:
+            process.run(f"fio {conf.name}")
