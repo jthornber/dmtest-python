e5701b31ab109152ae187764e72155652df4fb65
Ming-Hung Tsai <mtsai@redhat.com>
2024-07-30 15:52:10 +0800

    Add more thin_migrate negative tests

--- src/dmtest/thin_migrate/unit.py
+++ src/dmtest/thin_migrate/unit.py
@@ -42,11 +42,121 @@ def t_input_none_thin_device(fix):
         raise Exception("command succeeded without error")
 
 
+def t_device_not_present_in_metadata_snap(fix):
+    data_dev = fix.cfg["data_dev"]
+    thin_size = min(units.gig(1), utils.dev_size(data_dev) // 2)
+
+    with standard_pool(fix, block_size = 128, zero = True) as src_pool:
+        src_pool.message(0, f"reserve_metadata_snap")
+
+        with ps.new_thin(src_pool, thin_size, 0) as src_thin:
+            with ps.new_thin(src_pool, thin_size, 1) as dest_thin:
+                try:
+                    process.run(f"thin_migrate --source-dev {src_thin} --dest-dev {dest_thin}")
+                except subprocess.CalledProcessError:
+                    pass
+                except:
+                    raise
+                else:
+                    raise Exception("command succeeded without error")
+
+        src_pool.message(0, f"release_metadata_snap")
+
+
+def t_output_none_block_device(fix):
+    data_dev = fix.cfg["data_dev"]
+    thin_size = min(units.gig(1), utils.dev_size(data_dev) // 2)
+
+    with standard_pool(fix, block_size = 128, zero = True) as src_pool:
+        with ps.new_thin(src_pool, thin_size, 0) as src_thin:
+            src_pool.message(0, f"reserve_metadata_snap")
+            process.run(f"truncate migrate_dest --size {thin_size * 512}")
+
+            try:
+                process.run(f"thin_migrate --source-dev {src_thin} --dest-dev migrate_dest")
+            except subprocess.CalledProcessError:
+                pass
+            except:
+                raise
+            else:
+                raise Exception("command succeeded without error")
+
+            process.run(f"unlink migrate_dest")
+            src_pool.message(0, f"release_metadata_snap")
+
+
+def t_output_unsupported_file_type(fix):
+    data_dev = fix.cfg["data_dev"]
+    thin_size = min(units.gig(1), utils.dev_size(data_dev) // 2)
+
+    with standard_pool(fix, block_size = 128, zero = True) as src_pool:
+        with ps.new_thin(src_pool, thin_size, 0) as src_thin:
+            src_pool.message(0, f"reserve_metadata_snap")
+
+            try:
+                process.run(f"thin_migrate --source-dev {src_thin} --dest-file /dev/null")
+            except subprocess.CalledProcessError:
+                pass
+            except:
+                raise
+            else:
+                raise Exception("command succeeded without error")
+
+            src_pool.message(0, f"release_metadata_snap")
+
+
+def t_output_device_size_differs(fix):
+    data_dev = fix.cfg["data_dev"]
+    thin_size = min(units.gig(1), utils.dev_size(data_dev) // 2)
+
+    with standard_pool(fix, block_size = 128, zero = True) as src_pool:
+        with ps.new_thin(src_pool, thin_size, 0) as src_thin:
+            with ps.new_thin(src_pool, thin_size // 2, 1) as dest_thin:
+                src_pool.message(0, f"reserve_metadata_snap")
+
+                try:
+                    process.run(f"thin_migrate --source-dev {src_thin} --dest-dev {dest_thin}")
+                except subprocess.CalledProcessError:
+                    pass
+                except:
+                    raise
+                else:
+                    raise Exception("command succeeded without error")
+
+                src_pool.message(0, f"release_metadata_snap")
+
+
+def t_output_device_size_differs_in_file_mode(fix):
+    data_dev = fix.cfg["data_dev"]
+    thin_size = min(units.gig(1), utils.dev_size(data_dev) // 2)
+
+    with standard_pool(fix, block_size = 128, zero = True) as src_pool:
+        with ps.new_thin(src_pool, thin_size, 0) as src_thin:
+            with ps.new_thin(src_pool, thin_size // 2, 1) as dest_thin:
+                src_pool.message(0, f"reserve_metadata_snap")
+
+                try:
+                    process.run(f"thin_migrate --source-dev {src_thin} --dest-file {dest_thin}")
+                except subprocess.CalledProcessError:
+                    pass
+                except:
+                    raise
+                else:
+                    raise Exception("command succeeded without error")
+
+                src_pool.message(0, f"release_metadata_snap")
+
+
 def register(tests):
     tests.register_batch(
         "/thin_migrate/unit",
         [
             ("insufficient_buffer_size", t_insufficient_buffer_size),
             ("input_none_thin_device", t_input_none_thin_device),
+            ("device_not_present_in_metadata_snap", t_device_not_present_in_metadata_snap),
+            ("output_none_block_device", t_output_none_block_device),
+            ("output_unsupported_file_type", t_output_unsupported_file_type),
+            ("output_device_size_differs", t_output_device_size_differs),
+            ("output_device_size_differs_in_file_mode", t_output_device_size_differs_in_file_mode),
         ],
     )
