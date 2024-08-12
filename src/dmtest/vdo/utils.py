24ef5ae722b3d7b5c9777cde59bbe8642b5a921b
Ken Raeburn <raeburn@redhat.com>
2024-08-12 18:44:54 -0400

    New 'full' VDO tests.
    Tests VDO behavior around the point where the last available data
    block is allocated.
    
    Includes a new helper routine to efficiently trigger allocation of all
    the VDO block map tree pages without allocating data pages, and a
    debugging helper routine to pause the test and allow the developer to
    examine the Python state, examine the test devices, etc. Adds support
    for specifying the VDO slab size.

--- src/dmtest/vdo/utils.py
+++ src/dmtest/vdo/utils.py
@@ -1,9 +1,13 @@
 import dmtest.process as process
+from dmtest.utils import dev_size
 import dmtest.vdo.vdo_stack as vs
+import dmtest.vdo.stats as stats
 import dmtest.vdo.status as status
-import logging as log
 
+import code
 import json
+import logging as log
+from math import ceil
 import os
 import tempfile
 import time
@@ -56,24 +60,91 @@ def fsync(dev):
     with open(dev, 'w') as thing:
         os.fsync(thing.fileno())
 
-def run_fio(dev, size, offset, verify = False, stats = True, compression = 0):
+def run_fio_with_config(fio_config, raise_on_fail=True):
+    """Run fio with the specified config file content.
+
+    On success, return the parsed statistics output. On failure, throw
+    an exception if raise_on_fail or return the stderr content.
+    """
+    with tempfile.NamedTemporaryFile('w') as conf:
+        log.info("fio config:\n" + fio_config)
+        conf.write(fio_config)
+        conf.flush()
+        with tempfile.NamedTemporaryFile('w') as out:
+            cmd = f"fio {conf.name} --output={out.name} --output-format=json+"
+            results = process.run(cmd, raise_on_fail = raise_on_fail)
+            (return_code, stdout, stderr) = results
+            if return_code:
+                return stderr
+            with open(out.name, 'r') as fio_out_file:
+                log.info(fio_out_file.read())
+                fio_out_file.seek(0)
+                fio_out = json.load(fio_out_file)
+                written = fio_out['jobs'][0]['write']['io_bytes'] # bytes
+                duration = fio_out['jobs'][0]['write']['runtime'] # msec
+                log.info(f"wrote {written} bytes in {duration} msec")
+                return fio_out
+
+def run_fio(dev, size, offset, verify = False, stats = True, compression = 0, randseed = 1,
+            raise_on_fail = True):
+    """Run fio with the specified values.
+
+    On success, return the parsed statistics output if stats is
+    true. On failure, throw an exception if raise_on_fail or return
+    the stderr content.
+    """
+    maybe_verify = "verify_only" if verify else ""
     fio_config = fio_config_template.format(size=size,
                                             offset=offset,
                                             compress=compression,
                                             filename=str(dev),
-                                            maybe_verify = "verify_only" if verify else "")
-    log.info("fio config:\n" + fio_config)
-    with tempfile.NamedTemporaryFile('w') as conf:
-        conf.write(fio_config)
-        conf.flush()
-        if stats:
-            with tempfile.NamedTemporaryFile('w') as out:
-                process.run(f"fio {conf.name} --output={out.name} --output-format=json+")
-                fio_out = json.load(open(out.name, 'r'))
-                written = fio_out['jobs'][0]['write']['io_bytes'] # bytes
-                duration = fio_out['jobs'][0]['write']['runtime'] # msec
-                #log.info(fio_out)
-                log.info(f"wrote {written} bytes in {duration} msec")
-                return fio_out
-        else:
-            process.run(f"fio {conf.name}")
+                                            maybe_verify=maybe_verify)
+    fio_stats = run_fio_with_config(fio_config, raise_on_fail = raise_on_fail)
+    if stats:
+        return fio_stats
+
+block_map_entries_per_page = 812
+
+def populate_block_map(vdo_dev):
+    """Make sure the VDO device's block map has been fully allocated,
+    by writing zero blocks every so often to force population of the
+    tree.
+
+    """
+    vdo_size_sectors = dev_size(vdo_dev)
+    vdo_size_blocks = int(vdo_size_sectors / 8)
+    stats.vdo_stats(vdo_dev)
+    bm_leaf_pages = ceil(vdo_size_blocks / block_map_entries_per_page)
+    filename = str(vdo_dev)
+    populate_block_map_fio_config = f"""
+[stuff]
+bs=4k
+direct=0
+end_fsync=1
+filename={filename}
+group_reporting=1
+iodepth=128
+ioengine=libaio
+rw=write:{4*(block_map_entries_per_page-1)}k
+io_size={bm_leaf_pages*4}k
+size={int(vdo_size_sectors/2)}k
+thread=1
+zero_buffers=1
+"""
+    run_fio_with_config(populate_block_map_fio_config)
+
+# Useful while debugging tests: suspend execution and let the
+# developer examine Python variables, system state, etc.
+def repl(my_locals):
+    """Invoke a Python interactive session with access to the supplied
+    local variables. Intended for debugging tests, to pause execution
+    and allow examination of the test device and a test's internal
+    state.
+
+    Should be invoked as: repl(locals()).
+
+    On exiting the interactive session with ^D, returns to the caller.
+    """
+    variables = globals().copy()
+    variables.update(my_locals)
+    code.InteractiveConsole(variables).interact()
